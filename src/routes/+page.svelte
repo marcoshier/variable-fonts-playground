aefd80a012717946c55950be9257b17e94535ab0
marco <dellabatemarco@gmail.com>
2025-03-27 02:02:39 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Slider from "$lib/components/Slider.svelte";
+
+    const words = [
+        "carota",
+        "pisello",
+        "peperone",
+        "durian"
+    ]
+
+</script>
+
+{#each words as word}
+    <Slider word = { word }></Slider>
+{/each}
